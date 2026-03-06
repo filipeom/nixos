95914780b58520b8be30f391ac0fad9327da7dfd
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-03-06 14:56:10 +0000

    Add nvim module and manage nvim plugins with hm

--- dotfiles/nvim/lua/filipeom/plugins.lua
+++ dotfiles/nvim/lua/filipeom/plugins.lua
@@ -1,48 +1,48 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use 'EdenEast/nightfox.nvim'
-  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
-  use 'nvim-treesitter/playground'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-commentary'
-  use 'mason-org/mason.nvim'
-  -- use {
-  --   'VonHeikemen/lsp-zero.nvim',
-  --   branch = 'v2.x',
-  --   requires = {
-  --     -- LSP Support
-  --     { 'neovim/nvim-lspconfig' }, -- Required
-  --     {                          -- Optional
-  --       'williamboman/mason.nvim',
-  --       run = function()
-  --         pcall(vim.cmd, 'MasonUpdate')
-  --       end,
-  --     },
-  --     { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+-- return require('packer').startup(function(use)
+--   -- Packer can manage itself
+--   use 'wbthomason/packer.nvim'
+--   use {
+--     'nvim-telescope/telescope.nvim', tag = '0.1.8',
+--     requires = { { 'nvim-lua/plenary.nvim' } }
+--   }
+--   use 'EdenEast/nightfox.nvim'
+--   use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
+--   use 'nvim-treesitter/playground'
+--   use 'tpope/vim-fugitive'
+--   use 'tpope/vim-commentary'
+--   use 'mason-org/mason.nvim'
+--   -- use {
+--   --   'VonHeikemen/lsp-zero.nvim',
+--   --   branch = 'v2.x',
+--   --   requires = {
+--   --     -- LSP Support
+--   --     { 'neovim/nvim-lspconfig' }, -- Required
+--   --     {                          -- Optional
+--   --       'williamboman/mason.nvim',
+--   --       run = function()
+--   --         pcall(vim.cmd, 'MasonUpdate')
+--   --       end,
+--   --     },
+--   --     { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-  --     -- Autocompletion
-  --     -- Plug 'hrsh7th/cmp-nvim-lsp'
-  use { 'hrsh7th/cmp-buffer' }
-  use { 'hrsh7th/cmp-path' }
-  use { 'hrsh7th/cmp-cmdline' }
-  use { 'hrsh7th/nvim-cmp' }   -- Required
-  use { 'hrsh7th/cmp-nvim-lsp' } -- Required
-  use { 'hrsh7th/cmp-vsnip' }
-  use { 'hrsh7th/vim-vsnip' }
+--   --     -- Autocompletion
+--   --     -- Plug 'hrsh7th/cmp-nvim-lsp'
+--   use { 'hrsh7th/cmp-buffer' }
+--   use { 'hrsh7th/cmp-path' }
+--   use { 'hrsh7th/cmp-cmdline' }
+--   use { 'hrsh7th/nvim-cmp' }   -- Required
+--   use { 'hrsh7th/cmp-nvim-lsp' } -- Required
+--   use { 'hrsh7th/cmp-vsnip' }
+--   use { 'hrsh7th/vim-vsnip' }
 
-  --   }
-  -- }
-  use 'romgrk/barbar.nvim'
-  use 'nvim-tree/nvim-web-devicons'
-  use 'wasp-platform/ecmasl-vim'
-end)
+--   --   }
+--   -- }
+--   use 'romgrk/barbar.nvim'
+--   use 'nvim-tree/nvim-web-devicons'
+--   use 'wasp-platform/ecmasl-vim'
+-- end)
---
+++ modules/programs/nvim.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+
+let
+  ecmasl-vim = pkgs.vimUtils.buildVimPlugin {
+    name = "ecmasl-vim";
+    src = pkgs.fetchFromGitHub {
+      owner = "wasp-platform";
+      repo = "ecmasl-vim";
+      rev = "master";
+      sha256 = "sha256-R/8yvS8L+2MshS4e8RAn+E/k+qO9vM2GzNqI6X3V/7U="; # Placeholder, will update if needed
+    };
+  };
+in
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      # UI
+      nightfox-nvim
+      barbar-nvim
+      nvim-web-devicons
+
+      # Navigation & Editing
+      telescope-nvim
+      plenary-nvim
+      vim-fugitive
+      vim-commentary
+
+      # LSP & Completion
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp-cmdline
+      cmp-vsnip
+      vim-vsnip
+
+      # Treesitter
+      nvim-treesitter.withAllGrammars
+      playground
+
+      # Custom
+      # ecmasl-vim
+    ];
+
+    extraLuaConfig = ''
+      require('filipeom')
+    '';
+  };
+
+  # Symlink the lua configuration
+  xdg.configFile."nvim/lua".source = ../../dotfiles/nvim/lua;
+  xdg.configFile."nvim/after".source = ../../dotfiles/nvim/after;
+  xdg.configFile."nvim/lsp".source = ../../dotfiles/nvim/lsp;
+}
