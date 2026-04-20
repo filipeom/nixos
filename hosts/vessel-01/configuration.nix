50f1fdad44002c42248dc816b91e3f009ecf669e
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-04-20 13:56:49 +0100

    [vessel-XX] garbage collect old generations automatically

--- hosts/vessel-01/configuration.nix
+++ hosts/vessel-01/configuration.nix
@@ -252,4 +252,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
