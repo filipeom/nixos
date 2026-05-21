16591419e10831fdeedb61bddbf6d29c35f22654
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-05-21 23:26:50 +0100

    [vessel-01] attempt to reduce hyprlock crashes

--- hosts/vessel-01/home.nix
+++ hosts/vessel-01/home.nix
@@ -231,7 +231,26 @@
     };
   };
 
-  services.hypridle.enable = true;
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+      };
+      listener = [
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 900;
+          on-timeout="systemctl suspend";
+        }
+      ];
+    };
+  };
+
   services.hyprsunset.enable = true;
   services.hyprpaper.enable = true;
 
